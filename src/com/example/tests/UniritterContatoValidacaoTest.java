4e3ad2cea5feaebb16eb3f6c2373c1bafcdbdb21
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2016-03-25 16:53:46 -0300

    Adicionados arquivos do projeto java maven.

---
+++ src/com/example/tests/UniritterContatoValidacaoTest.java
@@ -0,0 +1,49 @@
+package com.example.tests;
+
+import org.junit.*;
+
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class UniritterContatoValidacaoTest {
+  //public UniritterContexto uniritterContexto;
+
+  @Test
+  public void test() throws Exception {
+    
+  }
+  
+  /*
+  @Before
+  public void setUp() throws Exception {
+	uniritterContexto = new UniritterContexto(new FirefoxDriver());
+  }
+
+  @Test
+  public void testUniritterContatoValidacaoError() throws Exception {
+    ContatoPage.irPara(uniritterContexto);
+	ContatoPage.enviarContato();
+    ContatoPage.validaRetornoErro();
+  }
+  
+  @Test
+  public void testUniritterContatoValidacaoSucesso() throws Exception {
+    ContatoPage.irPara(uniritterContexto);
+    ContatoPage.preencherCamposFormulario();
+	ContatoPage.enviarContatoPreenchido();
+    ContatoPage.validaRetornoSucesso();
+  }  
+  
+  @Test
+  public void testUniritterContatoValidacaoParcial() throws Exception {
+    ContatoPage.irPara(uniritterContexto);
+    ContatoPage.preencherParcialmenteCamposFormulario();
+	ContatoPage.enviarContatoPreenchido();
+    ContatoPage.validaRetornoErroParcial();
+  }    
+
+  @After
+  public void tearDown() throws Exception {
+	  uniritterContexto.quit();
+  }
+  */
+}
---
+++ src/com/example/tests/UniritterContexto.java
@@ -0,0 +1,24 @@
+package com.example.tests;
+
+//import java.util.concurrent.TimeUnit;
+
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class UniritterContexto {
+	/*
+	public WebDriver driver;
+	public String baseUrl;
+
+	public UniritterContexto(FirefoxDriver driver) {
+	    this.driver = driver;
+	    baseUrl = "http://www.uniritter.com.br/";
+	    
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+	
+	public void quit(){
+	    driver.quit();
+	}
+	*/
+}
