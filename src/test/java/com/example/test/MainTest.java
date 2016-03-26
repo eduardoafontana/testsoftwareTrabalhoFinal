d57e3abf738ad8c3bbc0d8506ca69cd7153ee1d9
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2016-03-26 15:46:04 -0300

    Atualizados para isolar problema de elemento não encontrado.

--- src/test/java/com/example/test/MainTest.java
+++ src/test/java/com/example/test/MainTest.java
@@ -7,38 +7,40 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MainTest {
 
-	public UniritterContexto uniritterContexto;
+	//public UniritterContexto uniritterContexto;
 	
-	@Before
-	public void setUp() throws Exception {
-		uniritterContexto = new UniritterContexto(new FirefoxDriver());
-	}
+//	@Before
+//	public void setUp() throws Exception {
+//		uniritterContexto = new UniritterContexto(new FirefoxDriver());
+//	}
 
 	@Test
 	public void testUniritterContatoValidacaoError() throws Exception {
-		ContatoPagina.irPara(uniritterContexto);
+		FirefoxDriver driver = new FirefoxDriver();
+		assertNotNull(driver);
+		//ContatoPagina.irPara(uniritterContexto);
 		//ContatoPagina.enviarContato();
 		//ContatoPagina.validaRetornoErro();
 	}
 
-	@Test
-	public void testUniritterContatoValidacaoSucesso() throws Exception {
-		ContatoPagina.irPara(uniritterContexto);
-		//ContatoPagina.preencherCamposFormulario();
-		//ContatoPagina.enviarContatoPreenchido();
-		//ContatoPagina.validaRetornoSucesso();
-	}
+//	@Test
+//	public void testUniritterContatoValidacaoSucesso() throws Exception {
+//		//ContatoPagina.irPara(uniritterContexto);
+//		//ContatoPagina.preencherCamposFormulario();
+//		//ContatoPagina.enviarContatoPreenchido();
+//		//ContatoPagina.validaRetornoSucesso();
+//	}
 
-	@Test
-	public void testUniritterContatoValidacaoParcial() throws Exception {
-		ContatoPagina.irPara(uniritterContexto);
-//		ContatoPagina.preencherParcialmenteCamposFormulario();
-//		ContatoPagina.enviarContatoPreenchido();
-//		ContatoPagina.validaRetornoErroParcial();
-	}
+//	@Test
+//	public void testUniritterContatoValidacaoParcial() throws Exception {
+//		//ContatoPagina.irPara(uniritterContexto);
+////		ContatoPagina.preencherParcialmenteCamposFormulario();
+////		ContatoPagina.enviarContatoPreenchido();
+////		ContatoPagina.validaRetornoErroParcial();
+//	}
 
-	@After
-	public void tearDown() throws Exception {
-		uniritterContexto.quit();
-	}
+//	@After
+//	public void tearDown() throws Exception {
+//		uniritterContexto.quit();
+//	}
 }
