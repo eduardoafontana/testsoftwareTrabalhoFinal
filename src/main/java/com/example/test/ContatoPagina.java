ab0c52038ae7967dcd4bc6db4df4352e0bde4c5c
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2016-03-26 14:40:26 -0300

    Adicionada unidade de teste.

---
+++ src/main/java/com/example/test/ContatoPagina.java
@@ -0,0 +1,114 @@
+package com.example.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ContatoPagina {
+
+	private static UniritterContexto contexto = null;
+	private static WebElement labelDeRetornoDeErroDoName;
+	private static WebElement labelDeRetornoDeErroDoEmail;
+	private static WebElement labelDeRetornoDeErroDoTelefone;
+	private static WebElement labelDeRetornoDeErroDoMensagem;
+	private static WebElement botaoDeEnviarDoFormulario;
+	private static WebElement campoNome;
+	private static WebElement campoEmail;
+	private static WebElement campoTelefone;
+	private static WebElement campoMensagem;
+	private static WebElement labelDeMensagemDeSucesso;
+
+	public static void irPara(UniritterContexto pContext) {
+		contexto = pContext;
+
+		contexto.driver.get(contexto.baseUrl);
+		contexto.driver.findElement(By.cssSelector("a[title=\"Fale Consoco\"]")).click();
+
+		mapearElementos();
+	}
+
+	private static void mapearElementos() {
+		botaoDeEnviarDoFormulario = contexto.driver.findElement(By.cssSelector("button.pull-left"));
+		campoNome = contexto.driver.findElement(By.id("SacNome"));
+		campoEmail = contexto.driver.findElement(By.id("SacEmail"));
+		campoTelefone = contexto.driver.findElement(By.id("SacTelefone"));
+		campoMensagem = contexto.driver.findElement(By.id("SacMensagem"));
+	}
+
+	public static void enviarContato() {
+		botaoDeEnviarDoFormulario.click();
+
+		mapearElementosRetornoValidacaoErro();
+	}
+	
+	public static void enviarContatoPreenchido() {
+		contexto.driver.findElement(By.id("SacAddForm")).submit();
+
+		mapearElementosRetornoValidacaoSucesso();
+	}	
+
+	private static void mapearElementosRetornoValidacaoErro() {
+		labelDeRetornoDeErroDoName = contexto.driver.findElement(By.cssSelector("label.error"));
+		labelDeRetornoDeErroDoEmail = contexto.driver.findElement(By.xpath("//form[@id='SacAddForm']/div[5]/label"));
+		labelDeRetornoDeErroDoTelefone = contexto.driver.findElement(By.xpath("//form[@id='SacAddForm']/div[6]/label"));
+		labelDeRetornoDeErroDoMensagem = contexto.driver.findElement(By.xpath("//form[@id='SacAddForm']/div[7]/label"));
+	}
+	
+	private static void mapearElementosRetornoValidacaoSucesso() {
+		labelDeMensagemDeSucesso = contexto.driver.findElement(By.id("flashMessage"));
+	}
+
+	public static String getNameError() {
+		return labelDeRetornoDeErroDoName.getText();
+	}
+
+	public static String getEmailError() {
+		return labelDeRetornoDeErroDoEmail.getText();
+	}
+
+	public static String getTelefoneError() {
+		return labelDeRetornoDeErroDoTelefone.getText();
+	}
+
+	public static String getMensagemError() {
+		return labelDeRetornoDeErroDoMensagem.getText();
+	}
+
+	public static void validaRetornoErro() {
+		assertEquals("Informe o seu nome", ContatoPagina.getNameError());
+		assertEquals("Informe o seu e-mail", ContatoPagina.getEmailError());
+		assertEquals("Informe o seu telefone", ContatoPagina.getTelefoneError());
+		assertEquals("Informe o sua mensagem", ContatoPagina.getMensagemError());
+	}
+
+	public static void preencherCamposFormulario() {
+		campoNome.clear();
+		campoNome.sendKeys("André");
+		campoEmail.clear();
+		campoEmail.sendKeys("andrefj@gmail.com");
+		campoTelefone.clear();
+		campoTelefone.sendKeys("(55) 9633-2211");
+		campoMensagem.clear();
+		campoMensagem.sendKeys("Lorem ipsum dolor sit amet, consectetur adipiscing elit.");	
+	}
+
+	public static void validaRetornoSucesso() {
+		assertEquals("Contato realizado com sucesso.", labelDeMensagemDeSucesso.getText());
+	}
+	
+	public static void preencherParcialmenteCamposFormulario() {
+		campoNome.clear();
+		campoNome.sendKeys("as");
+		campoEmail.clear();
+		campoEmail.sendKeys("ss@com.br");
+		campoTelefone.clear();
+		campoTelefone.sendKeys("(45) 5454-5");
+		campoMensagem.clear();
+		campoMensagem.sendKeys("sf");
+	}
+	
+	public static void validaRetornoErroParcial() {
+		assertEquals("O contato não pode ser salvo. Tente novamente.", labelDeMensagemDeSucesso.getText());
+	}
+}
--- src/test/java/com/example/test/MainTest.java
+++ src/test/java/com/example/test/MainTest.java
@@ -1,14 +1,44 @@
 package com.example.test;
 
+import org.junit.*;
 import static org.junit.Assert.*;
 
-import org.junit.Test;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class MainTest {
 
-	@Test
-	public void test() {
-		fail("O teste falhou mas executou!");
+	public UniritterContexto uniritterContexto;
+	
+	@Before
+	public void setUp() throws Exception {
+		uniritterContexto = new UniritterContexto(new FirefoxDriver());
 	}
 
+	@Test
+	public void testUniritterContatoValidacaoError() throws Exception {
+		ContatoPagina.irPara(uniritterContexto);
+		ContatoPagina.enviarContato();
+		ContatoPagina.validaRetornoErro();
+	}
+
+	@Test
+	public void testUniritterContatoValidacaoSucesso() throws Exception {
+		ContatoPagina.irPara(uniritterContexto);
+		ContatoPagina.preencherCamposFormulario();
+		ContatoPagina.enviarContatoPreenchido();
+		ContatoPagina.validaRetornoSucesso();
+	}
+
+	@Test
+	public void testUniritterContatoValidacaoParcial() throws Exception {
+		ContatoPagina.irPara(uniritterContexto);
+		ContatoPagina.preencherParcialmenteCamposFormulario();
+		ContatoPagina.enviarContatoPreenchido();
+		ContatoPagina.validaRetornoErroParcial();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		uniritterContexto.quit();
+	}
 }
