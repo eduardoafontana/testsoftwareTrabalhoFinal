b4420f987bd00076daea6845668917c74546d23c
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2016-03-27 15:08:19 -0300

    Adicionado get.

--- src/test/java/com/example/test/ContatoPagina.java
+++ src/test/java/com/example/test/ContatoPagina.java
@@ -4,8 +4,8 @@ import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.TimeUnit;
 
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 //import org.openqa.selenium.support.ui.ExpectedConditions;
 //import org.openqa.selenium.support.ui.WebDriverWait;
 
@@ -13,21 +13,22 @@ import java.util.concurrent.TimeUnit;
 public class ContatoPagina {
 
 	private static UniritterContexto contexto = null;
-//	private static WebElement labelDeRetornoDeErroDoName;
-//	private static WebElement labelDeRetornoDeErroDoEmail;
-//	private static WebElement labelDeRetornoDeErroDoTelefone;
-//	private static WebElement labelDeRetornoDeErroDoMensagem;
-//	private static WebElement botaoDeEnviarDoFormulario;
-//	private static WebElement campoNome;
-//	private static WebElement campoEmail;
-//	private static WebElement campoTelefone;
-//	private static WebElement campoMensagem;
-//	private static WebElement labelDeMensagemDeSucesso;
+	private static WebElement labelDeRetornoDeErroDoName;
+	private static WebElement labelDeRetornoDeErroDoEmail;
+	private static WebElement labelDeRetornoDeErroDoTelefone;
+	private static WebElement labelDeRetornoDeErroDoMensagem;
+	private static WebElement botaoDeEnviarDoFormulario;
+	private static WebElement campoNome;
+	private static WebElement campoEmail;
+	private static WebElement campoTelefone;
+	private static WebElement campoMensagem;
+	private static WebElement labelDeMensagemDeSucesso;
 
 	public static void irPara2(UniritterContexto pContext) {
 		contexto = pContext;
 
 		contexto.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		contexto.driver.get(contexto.baseUrl);
 	}
 
 
