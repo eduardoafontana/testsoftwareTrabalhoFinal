d42560cb386f19ce383298c8a06169223eb625e8
Eduardo Andreetta Fontana <fontanadnb@gmail.com>
2016-03-28 21:28:53 -0300

    Adicionado mapearElementos.

--- src/test/java/com/example/test/ContatoPagina.java
+++ src/test/java/com/example/test/ContatoPagina.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-
 public class ContatoPagina {
 
 	private static UniritterContexto contexto = null;
@@ -23,21 +22,28 @@ public class ContatoPagina {
 	private static WebElement campoTelefone;
 	private static WebElement campoMensagem;
 	private static WebElement labelDeMensagemDeSucesso;
+	private static WebDriverWait wait;
 
 	public static void irPara(UniritterContexto pContext) {
 		contexto = pContext;
 
 		contexto.driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		contexto.driver.get(contexto.baseUrl + "fale-conosco");
+
+		wait = new WebDriverWait(contexto.driver, 30);
+
+		mapearElementos();
+	}
+
+	private static void mapearElementos() {
+		botaoDeEnviarDoFormulario = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("button.pull-left")));
+		campoNome = wait.until(ExpectedConditions.presenceOfElementLocated(By.id("SacNome")));
 		
-		WebDriverWait wait = new WebDriverWait(contexto.driver, 60);
-		//WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[title=\"Fale Consoco\"]")));
+		//botaoDeEnviarDoFormulario = contexto.driver.findElement(By.cssSelector("button.pull-left"));
+		//campoNome = contexto.driver.findElement(By.id("SacNome"));
 		
-		//org.openqa.selenium.TimeoutException: Timed out after 500 seconds waiting for element to be clickable: By.cssSelector: a[title="Fale Consoco"]
-		//Testar ver se encontra outra coisa na página, dai pode-se evoluir
-		
-		WebElement element = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("a[class=\"logo\"]")));
-		
-		element.click();
+//		campoEmail = contexto.driver.findElement(By.id("SacEmail"));
+//		campoTelefone = contexto.driver.findElement(By.id("SacTelefone"));
+//		campoMensagem = contexto.driver.findElement(By.id("SacMensagem"));
 	}
 }
